0f4d567b6798fd8834cc1f8ee543bfb14f63b0ca
Maylee <mgagnon987@gmail.com>
2019-11-07 18:11:40 -0500

    clean up print statements

--- Square/src/Square.cpp
+++ Square/src/Square.cpp
@@ -7,19 +7,15 @@
 #include <vector>
 using namespace std;
 
-
-
 int main() {
-	cout << "Hello" << endl;
-
-	//total 132
 	int arr[16] = {1,14,14,4,11,7,6,9,8,10,10,5,13,2,3,15}; // initializing square
 	int tallies[133] = {0};
-	int sum = 33;
+	int sum = 33; // some of the rows etc
 	int tally4 = 0;
 	int tally33 = 0;
+	//total sum of the square 132
 
-
+	//creating the combinations
 	int n = 16;
 	vector<string> arrGroups;
 		arrGroups.push_back("0");
@@ -39,7 +35,7 @@ int main() {
 			}
 		}
 
-
+	// checking the sum each combination
 	for (i = 0; i < arrGroups.size(); i++) { // go through each combination
 		int groupSum = 0;
 		int numInGroup = 0;
@@ -51,8 +47,8 @@ int main() {
 			}
 		}
 
-		if (groupSum == 33) {
-			if (numInGroup == 4){
+		if (groupSum == sum) { // P1&P2 combination equal to 33
+			if (numInGroup == 4){ //P1 = 33 & 5 elements
 					tally4++;
 			}
 			tally33++;
@@ -60,11 +56,13 @@ int main() {
 		tallies[groupSum]++; //increment tally count for corresponding sum
 	} // end for loop going through each combination
 
-	cout << "tally4: " << tally4 << endl;
-	cout << "tally33: " << tally33 << endl;
+	//printing results
+	cout << "Num combinations =33 & 4 elements: " << tally4 << endl;
+	cout << "Num combinations =33: " << tally33 << endl;
 
 	for (int i = 0; i <= 132; i++){
 		cout << tallies[i] << " Number of combinations equal to " << i << endl;
 	}
+
 	return 0;
 }
