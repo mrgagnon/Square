46eda102adbd00959c12d73f3500b42aef1145f4
Maylee <mgagnon987@gmail.com>
2019-11-07 11:13:08 -0500

    1st commit

---
+++ Square/src/Square.cpp
@@ -0,0 +1,13 @@
+/* Maylee Gagnon
+ * CS 2223 proj2 p4 Square
+ * 11.8.2019
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello" << endl; // prints !!!Hello World!!!
+	return 0;
+}
