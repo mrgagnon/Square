cc1a3f79246a84087c344da99d463ed74b418f49
Maylee <mgagnon987@gmail.com>
2019-11-07 17:56:51 -0500

    p4 working

--- Square/src/Square.cpp
+++ Square/src/Square.cpp
@@ -12,7 +12,9 @@ using namespace std;
 int main() {
 	cout << "Hello" << endl;
 
+	//total 132
 	int arr[16] = {1,14,14,4,11,7,6,9,8,10,10,5,13,2,3,15}; // initializing square
+	int tallies[133] = {0};
 	int sum = 33;
 	int tally4 = 0;
 	int tally33 = 0;
@@ -55,22 +57,14 @@ int main() {
 			}
 			tally33++;
 		}
+		tallies[groupSum]++; //increment tally count for corresponding sum
 	} // end for loop going through each combination
 
 	cout << "tally4: " << tally4 << endl;
 	cout << "tally33: " << tally33 << endl;
 
-
-	/* Part C TODO
-	 * some way to track all the sums & corresponding tally [0,total of all] A large 2d array?
-	 * if sum present then corresponding tally++; // efficient way to search/where to add?
-	 * if new sum then add to list & tally=1;
-	 */
-
-	/* Part D TODO
-	 * Loop through tally list & Look for the most tallies
-	 * Get sum & tally count
-	 */
-
+	for (int i = 0; i <= 132; i++){
+		cout << tallies[i] << " Number of combinations equal to " << i << endl;
+	}
 	return 0;
 }
